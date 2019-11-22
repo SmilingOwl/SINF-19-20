d2f73ed969debd9074dab6f3c3830f733fac0bbf
Juliana Marques <julianamcm05@gmail.com>
2019-11-22 09:58:38 +0000

    Add connection between react and nodeJS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,14 +4,35 @@ import logo from './logo.svg';
 import './App.css';
 import AppRouter from './AppRouter';
 
-function App()
+class App extends Component
 {
-  return(
-    <div className="App">
-      <AppRouter />
-    </div>
-  )
-}
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
 
+  callAPI() {
+    fetch("http://localhost:9000/testAPI")
+        .then(res => res.text())
+        .then(res => this.setState({ apiResponse: res }))
+        .catch(err=>err);
+  }
+
+  componentWillMount() {
+    this.callAPI();
+  }
+
+  render(){
+      return(
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} classNames="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">;{this.state.apiResponse}</p>
+      </div> 
+    );
+  }
+  }
 
 export default App;
