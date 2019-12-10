f67588584aa09c61eab683e1d0c9b0a56e184615
tripor <figorifico3000@gmail.com>
2019-12-10 12:46:28 +0000

    small changes to login

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,63 +1,101 @@
+/* eslint-disable linebreak-style */
 import React, { useState } from 'react';
-import { Redirect } from "react-router-dom";
-import { useAuth } from "../context/auth";
+import { Redirect } from 'react-router-dom';
 import axios from 'axios';
+import { Spinner } from 'reactstrap';
+import { useAuth } from '../context/auth';
 
-function Login() {
-    const [isLoggedIn, setLoggedIn] = useState(false);
-    const [isError, setIsError] = useState(false);
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const { setAuthTokens } = useAuth();
+const Login = () => {
+  const [isLoggedIn, setLoggedIn] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { setAuthTokens } = useAuth();
 
-    function login() {
-      setIsError(false);
-      axios.post("http://localhost:9000/auth/login", {
+  const login = async () => {
+    setIsLoading(true);
+    setIsError(false);
+    await axios
+      .post('http://localhost:9000/auth/login', {
         email,
-        password
-      }).then(result => {
+        password,
+      })
+      .then((result) => {
         if (result.status === 200) {
           setAuthTokens(result.data);
           setLoggedIn(true);
         } else {
           setIsError(true);
         }
-      }).catch(e => {
+      })
+      .catch(() => {
         setIsError(true);
       });
-    }
+    setIsLoading(false);
+  };
 
-    if (isLoggedIn) {
-        return <Redirect to="/" />;
-    }
+  if (isLoggedIn) {
+    return <Redirect to="/" />;
+  }
 
-    return (
-        <div>
-          <div className="row mtop">
-            <div className="col-md-4"/>
-            <div className="col-md-4 smallBox padding-2">
-              <h3>Login</h3>
-              <hr className="padding-bottom-1"/>
-                <div className="padding-bottom-1">
-                  <label htmlFor="email">Email address</label>
-                  <input type="email" className="form-control" id="email" aria-describedby="emailHelp" placeholder="Enter email"
-                  onChange={e => {
-                    setEmail(e.target.value);
-                  }}/>
-                </div>
-                <div className="padding-bottom-1">
-                  <label htmlFor="password">Password</label>
-                  <input type="password" className="form-control" id="password" placeholder="Password"
-                  onChange={e => {
-                    setPassword(e.target.value);
-                  }}/>
-                </div>
-                { isError &&<div className="padding-bottom-1 alert alert-danger" role="alert">The credentials provided are invalid.</div> }
-                <button onClick={login} className="btn btn-primary">Login</button>
-            </div>
+  return (
+    <div>
+      <div className="row mtop">
+        <div className="col-md-4" />
+        <div className="col-md-4 smallBox padding-2">
+          <h3>Login</h3>
+          <hr className="padding-bottom-1" />
+          <div className="padding-bottom-1">
+            <label htmlFor="email">
+              Email address
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                aria-describedby="emailHelp"
+                placeholder="Enter email"
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+            </label>
           </div>
+          <div className="padding-bottom-1">
+            <label htmlFor="password">
+              Password
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                placeholder="Password"
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              />
+            </label>
+          </div>
+          {(() => {
+            if (isError) {
+              return (
+                <div className="padding-bottom-1 alert alert-danger" role="alert">
+                  The credentials provided are invalid.
+                </div>
+              );
+            }
+            return <></>;
+          })()}
+          {isLoading ? (
+            <Spinner />
+          ) : (
+            <button onClick={login} type="submit" className="btn btn-primary">
+              Login
+            </button>
+          )}
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Login;
